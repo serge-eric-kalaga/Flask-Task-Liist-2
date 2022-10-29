39c748260cd028510daa9019995d3ee1a3852a49
serge-eric-kalaga <kalagaserge4@gmail.com>
2022-10-29 13:08:10 +0000

    Init task model and form

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '12345678'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.db"
+
+db = SQLAlchemy(app)
 
 from routes import *
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+    from models import *
+    with app.app_context():
+        db.create_all()
--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+import forms
 
 @app.route("/")
 @app.route("/index")
@@ -7,6 +8,11 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/about")
+@app.route("/about", methods=['POST', 'GET'])
 def about():
-    return render_template('about.html')
+    form = forms.AddTaskForm()
+    title = None
+    
+    if form.validate_on_submit():
+        title = form.title.data
+    return render_template('about.html', form=form, title=title)
